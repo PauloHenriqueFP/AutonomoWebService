df0d43434c281f4ca095ee1d7b3c6064c2c53cc2
Paulo Patarello <paulohenriquepatarello@gmail.com>
2021-06-05 10:20:48 -0300

    Adicionando anotacao @Service

--- src/main/java/com/empresax/autonomo/security/MyUserDetailsService.java
+++ src/main/java/com/empresax/autonomo/security/MyUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.empresax.autonomo.model.User;
 import com.empresax.autonomo.repository.UserRepository;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
 	@Autowired
